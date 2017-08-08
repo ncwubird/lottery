0e12b75a6e6b80f536689e862bf5feb6ed71d694
zhuyangyang <987484322@qq.com>
2017-08-08 12:18:16 +0800

    第一次提交

---
+++ Lottery/Lottery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lottery
+//
+//  Created by 苟晓浪 on 2017/8/8.
+//  Copyright © 2017年 com.yunkanghuigu.lottery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
