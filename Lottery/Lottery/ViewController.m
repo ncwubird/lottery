e994887b9a882973b34b602d266a3335558ec613
zhuyangyang <987484322@qq.com>
2017-08-08 13:32:45 +0800

    ...

--- Lottery/Lottery/ViewController.m
+++ Lottery/Lottery/ViewController.m
@@ -17,16 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    //645984134684+95465434658413
-    ///23132123132132123123
-
-    
-    //645984134684+95465434658413
-    ///23132123132132123123
-    
-    //645984134684+95465434658413
-    ///23132123132132123123
-
 }
 
 
