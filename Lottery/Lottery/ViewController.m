3b3e0b83fb7aa29d01b6a6ba5962bf55fb34075f
zhuyangyang <987484322@qq.com>
2017-08-08 13:48:25 +0800

    99999

--- Lottery/Lottery/ViewController.m
+++ Lottery/Lottery/ViewController.m
@@ -26,6 +26,9 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    NSLog(@"123135");
+    NSLog(@"123135");
+    NSLog(@"123135");
 }
 
 
